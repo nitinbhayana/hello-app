64db70bf67ae8a943017f2e766c03dcaaaff1b84
nitinbhayana <142592459+nitinbhayana@users.noreply.github.com>
2024-05-14 14:36:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,10 @@ import streamlit as st
 
 import requests
 
-API_URL_ner = "https://api-inference.huggingface.co/models/shivanikerai/TinyLlama-1.1B-Chat-v1.0-sku-title-ner-generation-reversed-v1.0"
-API_URL_suggest = "https://api-inference.huggingface.co/models/nitinbhayana/TinyLlama-1.1B-Chat-v1.0-title-suggestion-v1.0"
+API_URL = "https://api-inference.huggingface.co/models/shivanikerai/TinyLlama-1.1B-Chat-v1.0-sku-title-ner-generation-reversed-v1.0"
 headers = {"Authorization": "Bearer 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"}
 
-def query(API_URL,payload):
+def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
@@ -20,7 +19,7 @@ def ner_for_title(title):
     B_in, E_in = "[Title]", "[/Title]"
     # Format your prompt template
     prompt = f"""{B_INST} {B_SYS} You are a helpful assistant that provides accurate and concise responses. {E_SYS}\nExtract named entities from the given product title. Provide the output in JSON format.\n{B_in} {title.strip()} {E_in}\n{E_INST}\n\n### NER Response:\n{{"{title.split()[0].lower()}"""
-    output = query(API_URL_ner,{
+    output = query({
     "inputs": prompt,
     "parameters": {"return_full_text":False,},
     "options":{"wait_for_model": True}
@@ -28,20 +27,6 @@ def ner_for_title(title):
 
     return eval('{"'+title.split()[0]+output[0]['generated_text'])
 
-
-def ner_for_title(title):
-    B_SYS, E_SYS = "<<SYS>>", "<</SYS>>"
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_in, E_in = "[Title]", "[/Title]"
-    # Format your prompt template
-    prompt =f"""<s>[INST] <<SYS>> You are a helpful assistant that provides accurate and concise responses. <</SYS>>\nCreate a new, easy to read, and error free title for a given Ecommerce product title.\n[Title] {title} [/Title]\n[/INST]\n### Suggested Title:"""# f"""{B_INST} {B_SYS} You are a helpful assistant that provides accurate and concise responses. {E_SYS}\nExtract named entities from the given product title. Provide the output in JSON format.\n{B_in} {title.strip()} {E_in}\n{E_INST}\n\n### NER Response:\n{{"{title.split()[0].lower()}"""
-    output = query(API_URL_suggest,{
-    "inputs": prompt,
-    "parameters": {"return_full_text":False,},
-    "options":{"wait_for_model": True}
-    })
-
-    return output[0]['generated_text']
 
 
 # Streamlit app layout
@@ -56,12 +41,10 @@ def main():
     if st.button("Submit"):
         # Perform NER on the input title
         ner_result = ner_for_title(title_input)
-
-
         #st.write(ner_result)
         # Display the title with NER annotations
         st.subheader("Artificial Intelligence")
-	
+        st.write("created by NB")
         
         # Start from the original title and replace phrases one by one
         annotated_title = title_input
@@ -80,17 +63,11 @@ def main():
         
         # Display the fully annotated title using HTML to allow styling
         st.markdown(annotated_title, unsafe_allow_html=True)
-	
-
         st.subheader("General Parameters")
         st.write("Length of Title           : ", len(title_input))
         st.write("Count of words            : ", len(title_input.split()))
         st.write("Count of attributes       : ", len(ner_result))
         st.write("Count of alpha-numeric    : ",sum(char.isalnum() for char in title_input))
         st.write("Count of non alpha-numeric: ",len(title_input)-sum(char.isalnum() or char == ' ' for char in title_input))
-
-
-
 if __name__ == "__main__":
     main()
-
