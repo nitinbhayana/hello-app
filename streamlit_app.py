0f15d6f2b826c585c3297241e3dbad76d43a1923
nitinbhayana <142592459+nitinbhayana@users.noreply.github.com>
2024-05-14 14:16:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,7 @@ def main():
         # Display the fully annotated title using HTML to allow styling
         st.markdown(annotated_title, unsafe_allow_html=True)
 	
-	suggest_result = suggest_title(title_input)
-	st.write(suggest_result)
+
         st.subheader("General Parameters")
         st.write("Length of Title           : ", len(title_input))
         st.write("Count of words            : ", len(title_input.split()))
